e7f45617829afa688f4d48609d6307dba03b81b7
Zzaosk130 <pawapol.benchanarongrat@gmail.com>
2026-02-08 18:07:26 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
     return () => clearInterval(interval)
   }, [])
 
-  if (isValentine) {
+  if (!isValentine) {
   return (
     <main className="min-h-screen flex items-center justify-center p-4 relative overflow-hidden">
       
